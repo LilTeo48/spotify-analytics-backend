1754f4d2ad84cb4e841eefff94e5f716963caa56
LilTeo48 <93098754+LilTeo48@users.noreply.github.com>
2026-06-06 17:24:51 -0400

    Add Docker and Docker Compose support

--- app/database/db.py
+++ app/database/db.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://tylerchadwick@localhost:5432/spotify_analytics"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://tylerchadwick@localhost:5432/spotify_analytics"
+)
 
 engine = create_engine(DATABASE_URL)
 
